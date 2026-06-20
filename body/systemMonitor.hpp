460d5ac086beeea645492ec557f04e7197c38c79
anonymmized <lih0deev@inbox.ru>
2026-06-21 02:21:22 +0300

    Added a function to send and receive TCP packet

--- body/systemMonitor.hpp
+++ body/systemMonitor.hpp
@@ -6,6 +6,7 @@
 inline constexpr int SEC_IN_DAY    = 86400;
 inline constexpr int SEC_IN_HOUR   = 3600;
 inline constexpr int SEC_IN_MINUTE = 60;
+inline constexpr int TIMEOUT_IN_MS = 1000;
 
 struct CpuTimes {
     unsigned long long user = 0, nice = 0, system = 0, idle = 0, iowait = 0, irq = 0, softirq = 0, steal = 0;
