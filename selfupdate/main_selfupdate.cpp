0afaa58255f3154b8cbbf7b4916041ec4684e008
anonymmized <lih0deev@inbox.ru>
2026-06-24 00:57:59 +0300

    selfupdate functions were created and started

--- selfupdate/main_selfupdate.cpp
+++ selfupdate/main_selfupdate.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 
 const std::string REPO_URL = "https://github.com/anonymmized/KARD.git";
+const std::string homePath = std::getenv("HOME");
+const std::string UPDATE_LOG_PATH = homePath + "/.kard/update.log";
 
 std::filesystem::path createTempDirectory() {
     return std::filesystem::temp_directory_path() / ("kard-update-" + std::to_string(getpid()));
@@ -19,7 +21,23 @@ int main() {
         std::cout << "clone failed\n";
         return 1;
     }
-    std::cout << "Repo cloned to: " << tempDirectory;
+    std::cout << "Repo cloned to: " << tempDirectory << '\n';
+    std::string commandToConfigureMake = "cmake -S " + tempDirectory.string() + " -B " + tempDirectory.string() + "/build";
+
+    if (std::system(commandToConfigureMake.c_str()) != 0) {
+        std::cout << "Can't configure make\n";
+        return 1;
+    }
+
+    std::string commandToMake = "cmake --build " + tempDirectory.string() + "/build > " + UPDATE_LOG_PATH + " 2>&1";
+    if (std::system(commandToMake.c_str()) != 0) {
+        std::cout << "there is a make problem. to see what happens whatch update.log\n";
+        return 1;
+    }
+    std::cout << "Project built in temporary directory\n";
+
+    std::filesystem::remove_all(tempDirectory);
+    std::cout << "Temporary directory removed\n";
     return 0;
 }
 
