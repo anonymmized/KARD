164dfab4747201e7138579b791e882ab310e2735
anonymmized <lih0deev@inbox.ru>
2026-06-24 23:49:59 +0300

    new string for test

--- core/kardcore.cpp
+++ core/kardcore.cpp
@@ -59,7 +59,7 @@ int main(int argc, char** argv) {
             return 0;
         }
         if (cmd == "--test") {
-            std::cout << "helloworld\n";
+            std::cout << "this is a test\n";
             return 0;
         }
         else {
