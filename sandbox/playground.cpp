ecb728c7704f1a9e5b38ca0aae389fcdf29d06f4
anonymmized <lih0deev@inbox.ru>
2026-08-17 23:36:15 +0300

    how it will take string in sandbox

---
+++ sandbox/playground.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::string userInput;
+    std::cout << "User: ";
+    std::cin >> userInput;
+    std::cout << "Output: " << userInput << '\n';
+    return 0;
+}
