2c8ae3538aa305f75f841034a65b0f2b37b5f679
anonymmized <lih0deev@inbox.ru>
2026-08-17 23:55:17 +0300

    added simple strings splitter

--- sandbox/playground.cpp
+++ sandbox/playground.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
+#include <string>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+struct TermSize {
+    int cols = 80;
+    int rows = 24;
+};
+
+TermSize getTermSize() {
+    TermSize termSize;
+    winsize ws{};
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == 0) {
+        termSize.cols = ws.ws_col;
+        termSize.rows = ws.ws_row;
+    } else {
+        perror("ioctl");
+    }
+    return termSize;
+}
+
+std::vector<std::string> splitInput(const std::string& userInput) {
+    TermSize termSize = getTermSize();
+    std::vector<std::string> splitedStrings;
+    std::string line;
+    for (char symbol : userInput) {
+        if (line.size() >= termSize.cols - 1) {
+            line += symbol;
+            splitedStrings.push_back(line);
+            line.clear();
+            continue;
+        }
+        line += symbol;
+        if (symbol == '\n' || symbol == '\r') {
+            splitedStrings.push_back(line);
+            line.clear();
+        }
+    }
+    return splitedStrings;
+}
 
 int main() {
+    int firstVisible;
     std::string userInput;
     std::cout << "User: ";
     std::cin >> userInput;
-    std::cout << "Output: " << userInput << '\n';
+    std::vector<std::string> splittedStrings = splitInput(userInput);
+    for (const auto& str : splittedStrings) {
+        std::cout << '\t' << str << '\n';
+    }
     return 0;
 }
